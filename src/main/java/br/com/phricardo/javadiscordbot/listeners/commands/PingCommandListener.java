45e03dc24aa4995370ca905017b72e01fe219518
Pedro Ricardo <brpedroricardo@gmail.com>
2024-02-11 17:16:01 -0300

    chore: add avoiding Overengineering

--- src/main/java/br/com/phricardo/javadiscordbot/listeners/commands/PingCommandListener.java
+++ src/main/java/br/com/phricardo/javadiscordbot/listeners/commands/PingCommandListener.java
@@ -1,6 +1,6 @@
 package br.com.phricardo.javadiscordbot.listeners.commands;
 
-import br.com.phricardo.javadiscordbot.commands.PingCommand;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.event.message.MessageCreateEvent;
@@ -11,12 +11,14 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PingCommandListener implements MessageCreateListener {
 
-  private final PingCommand pingCommand;
-
   @Override
   public void onMessageCreate(MessageCreateEvent event) {
     final Message message = event.getMessage();
     final Boolean isValidCommand = (Boolean) message.getContent().equalsIgnoreCase("!ping");
-    if (isValidCommand) pingCommand.execute(event);
+    if (isValidCommand) this.execute(event);
+  }
+
+  private void execute(@NonNull final MessageCreateEvent event) {
+    event.getChannel().sendMessage("Pong!");
   }
 }
