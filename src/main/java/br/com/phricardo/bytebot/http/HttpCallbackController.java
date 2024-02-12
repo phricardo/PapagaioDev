c597695dc933f2e9e7aa89583dc52f428ce1cd13
Pedro Ricardo <brpedroricardo@gmail.com>
2024-02-12 04:09:41 -0300

    feat: add draft send message from callback

--- src/main/java/br/com/phricardo/bytebot/http/HttpCallbackController.java
+++ src/main/java/br/com/phricardo/bytebot/http/HttpCallbackController.java
@@ -1,8 +1,13 @@
 package br.com.phricardo.bytebot.http;
 
+import static java.lang.String.join;
+
 import br.com.phricardo.bytebot.http.payload.Issue;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class HttpCallbackController {
 
+  private final DiscordApi discordApi;
+  private static final long channelId = 1199666411350540308L;
+
   @PostMapping("/callback/jobs")
-  public String getIssuesJobs(@RequestBody Issue issue) {
-    return issue.toString();
+  public Issue getIssuesJobs(@RequestBody final Issue issue) {
+    discordApi
+        .getTextChannelById(channelId)
+        .ifPresent(
+            channel -> {
+              channel.sendMessage(createEmbedMessage(issue));
+            });
+
+    return issue;
+  }
+
+  private EmbedBuilder createEmbedMessage(@NonNull final Issue issue) {
+    return new EmbedBuilder()
+        .setTitle(issue.getTitle())
+        .setDescription(issue.getBody())
+        .addField("Labels", join(", ", issue.getLabels()));
   }
 }
