f400f094cc5e93d1c9ba38ce9f8c5276ffeb2d73
Pedro Ricardo <brpedroricardo@gmail.com>
2024-02-12 04:36:34 -0300

    feat: add draft send message many channels

--- src/main/java/br/com/phricardo/bytebot/http/HttpCallbackController.java
+++ src/main/java/br/com/phricardo/bytebot/http/HttpCallbackController.java
@@ -1,8 +1,11 @@
 package br.com.phricardo.bytebot.http;
 
+import static br.com.phricardo.bytebot.utils.Constants.CUSTOM_COLOR;
 import static java.lang.String.join;
+import static java.util.List.of;
 
 import br.com.phricardo.bytebot.http.payload.Issue;
+import java.util.List;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,24 +21,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class HttpCallbackController {
 
   private final DiscordApi discordApi;
-  private static final long channelId = 1199666411350540308L;
+  private static final List<Long> channelIds = of(1199666411350540308L, 1018175254570487890L);
 
   @PostMapping("/callback/jobs")
   public Issue getIssuesJobs(@RequestBody final Issue issue) {
+    channelIds.forEach(channelId -> sendMessage(channelId, issue));
+    return issue;
+  }
+
+  private void sendMessage(@NonNull final Long channelId, @NonNull final Issue issue) {
     discordApi
         .getTextChannelById(channelId)
-        .ifPresent(
-            channel -> {
-              channel.sendMessage(createEmbedMessage(issue));
-            });
-
-    return issue;
+        .ifPresent(channel -> channel.sendMessage(createEmbedMessage(issue)));
   }
 
   private EmbedBuilder createEmbedMessage(@NonNull final Issue issue) {
     return new EmbedBuilder()
+        .setColor(CUSTOM_COLOR)
         .setTitle(issue.getTitle())
         .setDescription(issue.getBody())
-        .addField("Labels", join(", ", issue.getLabels()));
+        .addField("Skills", join(", ", issue.getLabels()));
   }
 }
